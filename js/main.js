9859ede6388d0edf50b0db6022cf3a1a4a1dd3d5
KarimMagdy96 <karimmagdy096@gmail.com>
2024-01-20 11:08:48 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var searchQueries = document.querySelectorAll(".searchQuerie");
+var titleHolder = document.getElementById("titleHolder");
+var recipesImg = document.querySelectorAll(".recipesImg");
+console.log(searchQueries, titleHolder.innerHTML, recipesImg);
