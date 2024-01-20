10c4103136e1260cba3c01fbe5a48e792cce51c4
KarimMagdy96 <karimmagdy096@gmail.com>
2024-01-20 14:17:51 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,41 @@
-var searchQueries = document.querySelectorAll(".searchQuerie");
+var searchQueries = Array.from(document.querySelectorAll(".searchQuerie"));
 var titleHolder = document.getElementById("titleHolder");
-var recipesImg = document.querySelectorAll(".recipesImg");
-console.log(searchQueries, titleHolder.innerHTML, recipesImg);
+var searchQuerieCard = Array.from(
+  document.querySelectorAll(".searchQuerieCard")
+);
+var searchQuerieCard = Array.from(
+  document.querySelectorAll(".searchQuerieCard")
+);
+
+var recipesImg = Array.from(document.querySelectorAll(".recipesImg"));
+var recipeName = "Pizza";
+for (var i = 0; i < searchQueries.length; i++) {
+  searchQueries[i].addEventListener("click", function (e) {
+    getRecipes(e.target.innerHTML);
+  });
+}
+
+for (var r = 0; r < searchQuerieCard.length; r++) {
+  searchQuerieCard[r].addEventListener("click", function (e) {
+    if (e.target.nextElementSibling.innerText != null) {
+      getRecipes(e.target.nextElementSibling.innerText);
+    }
+  });
+}
+
+// get all recipes
+async function getRecipes(category) {
+  var response = await fetch(
+    `https://forkify-api.herokuapp.com/api/search?q=${category}`
+  );
+  var Recipes = await response.json();
+  displayRecipes(Recipes.recipes);
+}
+// display recipes
+
+function displayRecipes(Recipes) {
+  for (var i = 0; i < Recipes.length; i++) {
+    recipesImg[i].src = Recipes[i].image_url;
+    recipesImg[i].alt = Recipes[i].title;
+  }
+}
